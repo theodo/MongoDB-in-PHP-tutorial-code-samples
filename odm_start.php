f8e66181affa9dbbfd0d83ba9ee0081002e54a14
Fabrice Bernhard <fabriceb@theodo.fr>
2012-01-31 17:04:35 +0100

    MacOsX

--- odm_start.php
+++ odm_start.php
@@ -2,10 +2,48 @@
 
 require "vendor/.composer/autoload.php";
 
-$config = new \Doctrine\MongoDB\Configuration();
-$config->setMetadataCacheImpl(new \Doctrine\Common\Cache\ArrayCache);
-$driverImpl = $config->newDefaultAnnotationDriver(array(__DIR__."/Documents"));
-$config->setMetadataDriverImpl($driverImpl);
-$config->setProxyDir(__DIR__ . '/Proxies');
+use Doctrine\Common\ClassLoader,
+    Doctrine\Common\Annotations\AnnotationReader,
+    Doctrine\ODM\MongoDB\DocumentManager,
+    Doctrine\MongoDB\Connection,
+    Doctrine\ODM\MongoDB\Configuration,
+    Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
+
+// Document classes
+$classLoader = new ClassLoader('Documents', __DIR__);
+$classLoader->register();;
+
+
+$cache = new \Doctrine\Common\Cache\ArrayCache;
+
+$config = new Configuration();
+$config->setMetadataCacheImpl($cache);
+
+$config->setProxyDir(__DIR__ . '/cache');
 $config->setProxyNamespace('Proxies');
-$dm = \Doctrine\ORM\DocumentManager::create($mongo, $config);
+
+$config->setHydratorDir(__DIR__ . '/cache');
+$config->setHydratorNamespace('Hydrators');
+
+$reader = new AnnotationReader();
+$reader->setDefaultAnnotationNamespace('Doctrine\ODM\MongoDB\Mapping\\');
+$config->setMetadataDriverImpl(new AnnotationDriver($reader, __DIR__ . '/Documents'));
+
+
+AnnotationDriver::registerAnnotationClasses();
+
+$dm = DocumentManager::create(new Connection(), $config);
+
+
+
+
+
+
+
+
+$newProject = new \Documents\Project('Another Project');
+
+$dm->persist($newProject);
+$dm->flush();
+
+var_dump($newProject);
